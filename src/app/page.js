29b1467d692c4a6c1a51dff8186c1064f591e99a
kiyotone <kunwarkirtan36@gmail.com>
2023-02-03 20:23:02 +0545

    pippys2ndpp

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,7 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>
+    <div className='text-3xl font-bold text-pink-400'>
       Pippy's PP
     </div>
   )
